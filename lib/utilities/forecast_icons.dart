1fbbbd3e80b238df37984a4803ef3be077f1606a
Rob Vandelinder <rob@robjvan.ca>
2021-12-18 08:51:06 -0330

    Added SVG icons

---
+++ lib/utilities/forecast_icons.dart
@@ -0,0 +1,12 @@
+class ForecastIcons {
+  static const String chevron_down = 'assets/icons/chevron-down.svg';
+  static const String chevron_up = 'assets/icons/chevron-up.svg';
+  static const String back = 'assets/icons/chevron-left.svg';
+  static const String settings = 'assets/icons/cog.svg';
+  static const String delete = 'assets/icons/delete.svg';
+  static const String list = 'assets/icons/format-list-bulleted.svg';
+  static const String search_location = 'assets/icons/map-search-outline.svg';
+  static const String add = 'assets/icons/plus.svg';
+  static const String radiobutton = 'assets/icons/radiobox-blank.svg';
+  static const String radiobutton_marked = 'assets/icons/radiobox-marked.svg';
+}
