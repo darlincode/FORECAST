ae70ad39a4f5e9dfae8a86debce8dc49639fbf1e
Rob Vandelinder <rob@robjvan.ca>
2021-12-19 08:52:06 -0330

    Added action exports

--- lib/actions/actions.dart
+++ lib/actions/actions.dart
@@ -1,9 +1,6 @@
 export 'loading_actions.dart';
-
-// FetchWeatherDataAction
-// ToggleAnimatedBackgroundsAction
-// ToggleDarkModeAction
-// AddLocationAction
-// RemoveLocationAction
-// ClearLocationsAction
-// SetCurrentLocationAction
+export 'location_actions.dart';
+export 'user_settings_actions.dart';
+export 'weather_state_actions.dart';
+export 'connectivity_actions.dart';
+export 'navigation_actions.dart';
