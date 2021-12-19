7fc9c4e77f65d843604ea682c550e6732538bf5e
Rob Vandelinder <rob@robjvan.ca>
2021-12-18 22:23:35 -0330

    Created placeholder file

---
+++ lib/actions/actions.dart
@@ -0,0 +1,23 @@
+export 'loading_actions.dart';
+
+
+// FetchWeatherDataAction
+// AddLocationAction
+// RemoveLocationAction
+// ClearLocationsAction
+// SetCurrentLocationAction
+// SetTempUnitsToCAction
+// SetTempUnitsToFAction
+// SetTempUnitsToKAction
+// SetWindSpeedUnitsToKmhAction
+// SetWindSpeedUnitsToMphAction
+// SetWindSpeedUnitsToKnotsAction
+// SetWindSpeedUnitsToMpsAction
+// SetAirPressureUnitsToKpaAction
+// SetAirPressureUnitsToMbarAction
+// SetAirPressureUnitsToInchesAction
+// SetAQIUnitsTo Action
+// SetAQIUnitsTo Action
+// SetAQIUnitsTo Action
+// ToggleAnimatedBackgroundsAction
+// ToggleDarkModeAction
