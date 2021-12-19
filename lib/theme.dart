c881d6b480f78e5716d44e85ecb1ada1303438f9
Rob Vandelinder <rob@robjvan.ca>
2021-12-18 22:22:59 -0330

    Started basic theme file

---
+++ lib/theme.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const Color white = Color(0xFFFFFFFF);
+const Color black = Color(0xFF000000);
+const Color green = Color(0xFF00FF00);
+const Color blue = Color(0xFF0000FF);
+const Color red = Color(0xFFFF0000);
+
+const Color textColorLightMode = Color(0xFF101010);
+const Color textColorDarkMode = Color(0xFFDDDDDD);
+
+const Color bgColorLightMode = Color(0xFFFFFFFF);
+const Color bgColorDarkMode = Color(0xFF151515);
+
+const Color cardColorLightMode = Color(0xFFAAAAAA);
+const Color cardColorDarkMode = Color(0xFF000000);
