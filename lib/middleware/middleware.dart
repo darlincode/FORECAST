e9e7af6c1331ad280625784c0cd0674821fb8542
Rob Vandelinder <rob@robjvan.ca>
2021-12-19 13:17:50 -0330

    Added method placeholders

--- lib/middleware/middleware.dart
+++ lib/middleware/middleware.dart
@@ -3,17 +3,25 @@ import 'package:fancy_weather/api/apis.dart';
 import 'package:fancy_weather/state.dart';
 import 'package:flutter/material.dart';
 import 'package:redux/redux.dart';
-import 'package:fancy_weather/middleware/weather_middleware.dart' as Weather;
+import 'package:fancy_weather/middleware/weather_middleware.dart' as weather;
 import 'package:fancy_weather/middleware/navigation_middleware.dart'
-    as Navigation;
+    as navigation;
 
 List<Middleware<GlobalAppState>> buildGlobalAppStateMiddleware({
   @required WeatherAPI weatherApi,
 }) {
   return [
     TypedMiddleware<GlobalAppState, FetchWeatherDataAction>(
-        Weather.handleWeatherDataRequest(weatherApi)),
+        weather.handleWeatherDataRequest(weatherApi)),
     TypedMiddleware<GlobalAppState, NavigateBodyAction>(
-        Navigation.buildHandleNavigateBodyAction()),
+        navigation.buildHandleNavigateBodyAction()),
   ];
+}
+
+void saveToPrefs(GlobalAppState state) async {
+  //
+}
+
+Future<GlobalAppState> loadFromPrefs(GlobalAppState state) async {
+  //
 }
