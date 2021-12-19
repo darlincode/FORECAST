7f9e3c60853d08134935a183ed488da92f9ceaeb
Rob Vandelinder <rob@robjvan.ca>
2021-12-19 08:54:27 -0330

    Created WeatherAPIProvider

---
+++ lib/providers/weather_api_provider.dart
@@ -0,0 +1,13 @@
+import 'package:http/http.dart' as http;
+import 'package:fancy_weather/config/config.dart' as config;
+
+class WeatherAPIProvider {
+  final String _apiUrl = 'v1/forecast.json?key=${config.getAPIKey()}&q=';
+  WeatherAPIProvider();
+
+  Future<http.Response> getWeatherDataFromApi(double lat, double long) {
+    return http.get(config
+        .getAPIUrl()
+        .resolve('$_apiUrl$lat,$long&days=5&aqi=yes&alerts=yes'));
+  }
+}
