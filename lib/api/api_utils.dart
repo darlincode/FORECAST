3df015d78e786040a5df7dbafb226db7799421a8
Rob Vandelinder <rob@robjvan.ca>
2021-12-19 08:53:00 -0330

    Added APIResponse model

---
+++ lib/api/api_utils.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class APIResponse<T> {
+  final bool isOk;
+  final String message;
+  final T result;
+  final int status;
+
+  APIResponse({
+    @required this.isOk,
+    @required this.message,
+    @required this.result,
+    @required this.status,
+  });
+
+  @override
+  String toString() =>
+      'APIResponse{isOk: $isOk, status: $status, message: $message, result: $result}';
+}
