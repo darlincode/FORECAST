a6c0876d8dbf5539c088cf88edd693cbb52b2060
Rob Vandelinder <rob@robjvan.ca>
2021-12-19 21:48:10 -0330

    Fixed reducer calls

--- lib/reducers/reducers.dart
+++ lib/reducers/reducers.dart
@@ -1,14 +1,16 @@
+import 'package:fancy_weather/reducers/data_reducers/data_reducers.dart';
 import 'package:fancy_weather/state.dart';
 
 GlobalAppState globalAppStateReducer(GlobalAppState state, action) {
   return GlobalAppState(
     connectionStatus: connectionStatusReducer(state.connectionStatus, action),
     loadingStatus: loadingStatusReducer(state.loadingStatus, action),
-    userSettings: userSettingsReducer(state.userSettings, action),
-    currentLocInfo: currentLocationReducer(state.currentLocInfo, action),
     locations: locationListReducer(state.locations, action),
-    activeWeatherAlerts:
-        activeWeatherAlertsReducer(state.activeWeatherAlerts, action),
-        weatherData: weatherDataReduer(state.weatherData, action),
+    userSettings: settingsStateReducer(state.userSettings, action),
+    currentLocInfo: currentLocationReducer(state.currentLocInfo, action),
+    weatherData: weatherDataReducer(state.weatherData, action),
   );
+
+
+  
 }
