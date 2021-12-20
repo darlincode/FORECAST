ddd3308be8c27c15439866e6ad26ba82b87ab18b
Rob Vandelinder <rob@robjvan.ca>
2021-12-19 21:48:33 -0330

    Added state reducers

---
+++ lib/reducers/data_reducers/data_reducers.dart
@@ -0,0 +1,6 @@
+export 'connection_status_reducer.dart';
+export 'current_loc_info_reducer.dart';
+export 'loading_status_reducer.dart';
+export 'locations_reducer.dart';
+export 'settings_state_reducer.dart';
+export 'weather_state_reducer.dart';
