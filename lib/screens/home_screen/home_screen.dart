4c41e387c48c49a79a963ec9091192d64bef8a83
Rob Vandelinder <rob@robjvan.ca>
2021-12-19 21:49:45 -0330

    Added temporary widgets for testing

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -1,5 +1,8 @@
+import 'package:fancy_weather/actions/actions.dart';
+import 'package:fancy_weather/models/models.dart';
 import 'package:fancy_weather/screens/home_screen/home_screen_view_model.dart';
 import 'package:fancy_weather/state.dart';
+import 'package:fancy_weather/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:redux/redux.dart';
@@ -12,6 +15,11 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  bool _isLoadingTest;
+
+  void onInit(Store<GlobalAppState> store) {
+    _isLoadingTest = store.state.loadingStatus == LoadingStatus.Loading;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,13 +27,41 @@ class _HomeScreenState extends State<HomeScreen> {
       distinct: true,
       converter: (Store<GlobalAppState> store) =>
           HomeScreenViewModel.create(store),
+      onInit: onInit,
       builder: (context, viewModel) {
+        _isLoadingTest = viewModel.isLoading;
         return Scaffold(
+          appBar: AppBar(
+            actions: [
+              IconButton(
+                icon: Icon(_isLoadingTest ? Icons.toggle_off : Icons.toggle_on),
+                onPressed: () {
+                  if (_isLoadingTest) {
+                    viewModel.dispatch(
+                      SetLoadingStatusAction(LoadingStatus.Idle),
+                    );
+                  } else {
+                    viewModel.dispatch(
+                      SetLoadingStatusAction(LoadingStatus.Loading),
+                    );
+                  }
+                },
+              )
+            ],
+          ),
           key: _scaffoldKey,
           body: viewModel.isLoading
-              ? CircularProgressIndicator()
+              ? Center(
+                  child: CircularProgressIndicator(color: AppTheme.green),
+                )
               : Center(
-                  child: Text('Home Screen'),
+                  child: Text(
+                    'Home Screen',
+                    style: TextStyle(
+                      color: Colors.green,
+                      fontSize: 32,
+                    ),
+                  ),
                 ),
         );
       },
