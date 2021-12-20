bc01f17bc263b34193038f4013297d8994ed0af2
Rob Vandelinder <rob@robjvan.ca>
2021-12-19 21:43:40 -0330

    Added LocationsScreen

---
+++ lib/screens/locations_screen/locations_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LocationsScreen extends StatefulWidget {
+  const LocationsScreen({Key key}) : super(key: key);
+
+  @override
+  _LocationsScreenState createState() => _LocationsScreenState();
+}
+
+class _LocationsScreenState extends State<LocationsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        //
+        );
+  }
+}
