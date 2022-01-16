5388b830f3d21e5c90847946ef2741bac1d04952
Rob Vandelinder <robv@rappid.design>
2022-01-15 22:57:30 -0330

    Added model exports

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -1,11 +1,24 @@
 export 'entity.dart';
-export 'loading_status_model.dart';
-export 'connection_status_model.dart';
-export 'location_model.dart';
-export 'air_pressure_unit_model.dart';
-export 'temp_unit_model.dart';
-export 'wind_speed_unit_model.dart';
-export 'weather_alert_model.dart';
 
-export 'repositories/settings_state.dart';
+export 'data_models/api_alert_model.dart';
+export 'data_models/api_astro_model.dart';
+export 'data_models/api_aqi_model.dart';
+export 'data_models/api_condition_model.dart';
+export 'data_models/api_current_conditions_model.dart';
+export 'data_models/api_day_model.dart';
+export 'data_models/api_forecast_model.dart';
+export 'data_models/api_forecastday_model.dart';
+export 'data_models/api_hour_model.dart';
+export 'data_models/api_location_model.dart';
+export 'data_models/simple_location_model.dart';
 export 'repositories/weather_state.dart';
+
+export 'enums/air_pressure_unit_model.dart';
+export 'enums/connection_status_model.dart';
+export 'enums/loading_status_model.dart';
+export 'enums/temp_unit_model.dart';
+export 'enums/wind_speed_unit_model.dart';
+
+export 'repositories/locations_list_state.dart';
+export 'repositories/settings_state.dart';
+export 'repositories/weather_data_list_state.dart';
