2260d4afea5c3ceaa06a44f1693dff098cffb0f3
Rob Vandelinder <robv@rappid.design>
2022-01-15 22:43:40 -0330

    Deleted unused files

--- lib/actions/loading_actions.dart
+++
@@ -1,8 +0,0 @@
-import 'package:fancy_weather/models/models.dart';
-import 'package:flutter/material.dart';
-
-@immutable
-class SetLoadingStatusAction {
-  final LoadingStatus loadingStatus;
-  SetLoadingStatusAction(this.loadingStatus);
-}
