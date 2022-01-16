6da8176deb23193db3ed077b4e567df29a143bdf
Rob Vandelinder <robv@rappid.design>
2022-01-15 22:55:08 -0330

    Added placeholder files

---
+++ lib/widgets/hourly_forecast_panel.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HourlyForecastPanel extends StatelessWidget {
+  const HourlyForecastPanel({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
