325ffa1e6bd6c0d5db1e5a2794c5ee591f60ffbd
Rob Vandelinder <robv@rappid.design>
2022-01-25 17:03:43 -0330

    Moved user setting middleware

--- lib/middleware/middleware.dart
+++ lib/middleware/middleware.dart
@@ -8,6 +8,9 @@ import 'package:fancy_weather/middleware/weather_middleware.dart' as Weather;
 //     as Navigation;
 import 'package:fancy_weather/middleware/current_location_middleware.dart'
     as Location;
+import 'package:fancy_weather/middleware/local_io_middleware.dart' as LocalIO;
+import 'package:fancy_weather/middleware/user_settings_middleware.dart'
+    as Settings;
 
 List<Middleware<GlobalAppState>> buildGlobalAppStateMiddleware({
   @required WeatherAPI weatherApi,
@@ -16,70 +19,17 @@ List<Middleware<GlobalAppState>> buildGlobalAppStateMiddleware({
     TypedMiddleware<GlobalAppState, FetchWeatherDataAction>(
         Weather.handleWeatherDataRequest(weatherApi)),
     TypedMiddleware<GlobalAppState, ToggleAnimatedBackgroundsAction>(
-        _handleToggleAnimatedBackgroundsRequest()),
+        Settings.handleToggleAnimatedBackgroundsRequest()),
     TypedMiddleware<GlobalAppState, ToggleDarkModeAction>(
-        _handleToggleDarkModeRequest()),
-    // TypedMiddleware<GlobalAppState, LoadLocalDataAction>(
-    //     handleLoadDataRequest()),
-    // TypedMiddleware<GlobalAppState, GrabCurrentLocationAction>(
-    //     Location.handleGetCurrentLocationRequest()),
+        Settings.handleToggleDarkModeRequest()),
+    TypedMiddleware<GlobalAppState, LoadLocalDataAction>(
+        LocalIO.handleLoadLocalDataRequest()),
+    TypedMiddleware<GlobalAppState, SaveLocalDataAction>(
+        LocalIO.handleSaveLocalDataRequest()),
+    TypedMiddleware<GlobalAppState, GrabCurrentCoordsAction>(
+        Location.handleGetCurrentLocationRequest()),
   ];
 }
-
-Middleware<GlobalAppState> _handleToggleAnimatedBackgroundsRequest() {
-  return TypedMiddleware<GlobalAppState, ToggleAnimatedBackgroundsAction>(
-      (store, action, next) {
-    store.dispatch(
-      UpdateUserSettingsAction(
-        store.state.userSettings.copyWith(
-          useAnimatedBackgrounds:
-              !store.state.userSettings.useAnimatedBackgrounds,
-        ),
-      ),
-    );
-  });
-}
-
-Middleware<GlobalAppState> _handleToggleDarkModeRequest() {
-  return TypedMiddleware<GlobalAppState, ToggleDarkModeAction>(
-      (Store<GlobalAppState> store, ToggleDarkModeAction action,
-          NextDispatcher next) {
-    store.dispatch(
-      UpdateUserSettingsAction(
-        store.state.userSettings.copyWith(
-          useDarkMode: !store.state.userSettings.useDarkMode,
-        ),
-      ),
-    );
-  });
-}
-
-// Middleware<GlobalAppState> handleLoadDataRequest() {
-//   return TypedMiddleware<GlobalAppState, LoadLocalDataAction>(
-//       (store, action, next) {
-//     /// Check for local data
-//     /// if not found, treat as first run
-//     /// grab user location
-//     try {
-//       // loadLocalData();
-//     } catch (e) {
-//       /// No local data available - treat as first run.
-//       /// 1. Grab User Coords
-//       dynamic _currentLoc = handleGrabCurrentCoordinates();
-
-//       /// 2. Save coords to userSettings.locationList[0]
-//       store.dispatch(
-//           AddLocationToSimpleListAction(_currentLoc.lat, _currentLoc.long));
-
-//       /// 3. Grab Weather data using coords
-//       store.dispatch(FetchWeatherDataAction(index: 0));
-
-//       /// 4. Save weather data to weatherLocationList[0]?
-//     }
-
-//     /// 5. Fire reducer to refresh UI
-//   });
-// }
 
 // void saveToPrefs(GlobalAppState state) async {
 //   //
