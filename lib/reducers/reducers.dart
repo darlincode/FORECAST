504400c229632cd3d7b199d9f7a790adbda39c70
Rob Vandelinder <robv@rappid.design>
2022-01-25 17:02:06 -0330

    Fixed globalAppStateReducer

--- lib/reducers/reducers.dart
+++ lib/reducers/reducers.dart
@@ -1,13 +1,20 @@
+import 'package:fancy_weather/actions/actions.dart';
 import 'package:fancy_weather/reducers/data_reducers/data_reducers.dart';
 import 'package:fancy_weather/state.dart';
 
-GlobalAppState globalAppStateReducer(GlobalAppState state, action) {
-  return GlobalAppState(
-    activeLocationIndex:
-        activeLocationReducer(state.activeLocationIndex, action),
-    loadingStatus: loadingStatusReducer(state.loadingStatus, action),
-    locationList: locationListReducer(state.locationList, action),
-    weatherDataList: weatherDataListReducer(state.weatherDataList, action),
-    userSettings: settingsStateReducer(state.userSettings, action),
-  );
+GlobalAppState globalAppStateReducer(GlobalAppState state, action) =>
+    (action is SetGlobalStateAction)
+        ? setGlobalStateReducer(state, action)
+        : state.copyWith(
+            activeLocationIndex:
+                activeLocationReducer(state.activeLocationIndex, action),
+            loadingStatus: loadingStatusReducer(state.loadingStatus, action),
+            locationList: locationListReducer(state.locationList, action),
+            weatherDataList:
+                weatherDataListReducer(state.weatherDataList, action),
+            userSettings: settingsStateReducer(state.userSettings, action),
+          );
+
+setGlobalStateReducer(GlobalAppState state, SetGlobalStateAction action) {
+  return action.state;
 }
