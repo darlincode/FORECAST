589812cf9e59823e9affeb2b5d3b21cf296f8f69
Rob Vandelinder <robv@rappid.design>
2022-01-25 17:01:44 -0330

    Fixed typo

--- lib/reducers/data_reducers/loading_status_reducer.dart
+++ lib/reducers/data_reducers/loading_status_reducer.dart
@@ -7,7 +7,7 @@ Reducer<LoadingStatus> loadingStatusReducer =
         _setLoadingStatusReducer);
 
 LoadingStatus _setLoadingStatusReducer(
-    LoadingStatus loadingStatus, SetLoadingStatusAction action) {
+    LoadingStatus status, SetLoadingStatusAction action) {
   print('DEBUG: LoadingStatus = ${action.loadingStatus}');
 
   return action.loadingStatus;
