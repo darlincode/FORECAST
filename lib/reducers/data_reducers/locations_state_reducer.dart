4c47786fb9f214ab8be7510126cc61f0753561fb
Rob Vandelinder <robv@rappid.design>
2022-01-25 17:00:14 -0330

    Added ReplaceCoordsInLocationListAction reducer

--- lib/reducers/data_reducers/locations_state_reducer.dart
+++ lib/reducers/data_reducers/locations_state_reducer.dart
@@ -1,11 +1,12 @@
 import 'package:fancy_weather/actions/actions.dart';
 import 'package:fancy_weather/models/models.dart';
 import 'package:redux/redux.dart';
+import 'package:uuid/uuid.dart';
 
 Reducer<int> activeLocationReducer =
     TypedReducer<int, SetActiveLocationIndexAction>(_setActiveLocationReducer);
 
-int _setActiveLocationReducer(int index, SetActiveLocationIndexAction action) {
+int _setActiveLocationReducer(int state, SetActiveLocationIndexAction action) {
   print('DEBUG: _setActiveLocationReducer fired');
 
   return action.index;
@@ -23,6 +24,22 @@ List<SimpleLocation> locationListReducer(List<SimpleLocation> state, action) {
           ),
         ),
     );
+  }
+
+  if (action is ReplaceCoordsInLocationListAction) {
+    SimpleLocation _newLoc = SimpleLocation(
+        id: Uuid().v4(),
+        latitude: action.latitude,
+        longitude: action.longitude);
+    if (List.from(state).isEmpty) {
+      return List.unmodifiable([_newLoc]);
+      // ..removeAt(action.index)
+      // ..insert(action.index, _newLoc);
+    } else {
+      return List.from(state)
+        ..removeAt(action.index)
+        ..insert(action.index, _newLoc);
+    }
   }
 
   if (action is UpdateLocationListAction) {
