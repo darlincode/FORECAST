a8169028d85bc02c589e2888f7e7d9097560bc70
Rob Vandelinder <robv@rappid.design>
2022-02-19 10:08:09 -0330

    Added logo and title to splash screen

--- lib/screens/home_screen/splash.dart
+++ lib/screens/home_screen/splash.dart
@@ -1,27 +1,68 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:fancy_weather/state.dart';
+import 'package:fancy_weather/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
 
+@immutable
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Column(mainAxisSize: MainAxisSize.min, children: [
-      Image.asset(
-        'assets/images/logo/app_icon.png',
-        height: 128,
-      ),
-      const SizedBox(height: 32),
-      Text(
-        tr('loading'),
-        style: TextStyle(fontSize: 24, fontFamily: 'CarroisSC'),
-      ),
-      const SizedBox(height: 16),
-      const SizedBox(
-          height: 48,
-          width: 48,
-          child: Center(child: CircularProgressIndicator()))
-    ]));
+    final double _sw = MediaQuery.of(context).size.width;
+    final double _sh = MediaQuery.of(context).size.height;
+    return StoreConnector<GlobalAppState, SplashViewModel>(
+      distinct: true,
+      converter: (Store<GlobalAppState> store) => SplashViewModel.create(store),
+      builder: (context, vm) {
+        return Container(
+          width: _sw,
+          height: _sh,
+          color: vm.bgColor,
+          child: Center(
+              child: Column(mainAxisSize: MainAxisSize.min, children: [
+            Image.asset(
+              'assets/images/logo/app_icon.png',
+              height: 128,
+            ),
+            const SizedBox(height: 32),
+            Text(
+              tr('loading'),
+              style: vm.loadingTextStyle,
+            ),
+            const SizedBox(height: 16),
+            const SizedBox(
+                height: 48,
+                width: 48,
+                child: Center(child: CircularProgressIndicator()))
+          ])),
+        );
+      },
+    );
+  }
+}
+
+@immutable
+class SplashViewModel {
+  final Color bgColor;
+  final TextStyle loadingTextStyle;
+
+  SplashViewModel({
+    @required this.bgColor,
+    @required this.loadingTextStyle,
+  });
+
+  factory SplashViewModel.create(Store<GlobalAppState> store) {
+    return SplashViewModel(
+      bgColor: store.state.userSettings.useDarkMode
+          ? bgColorDarkMode
+          : bgColorLightMode,
+      loadingTextStyle: TextStyle(
+          fontSize: 24,
+          fontFamily: 'CarroisSC',
+          color: store.state.userSettings.useDarkMode ? white : black),
+    );
   }
 }
