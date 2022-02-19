d500651b80c73e1f9c980c2b40d635c70467c91b
Rob Vandelinder <robv@rappid.design>
2022-02-19 09:58:05 -0330

    Added API/AQI card export

--- lib/widgets/cards/cards.dart
+++ lib/widgets/cards/cards.dart
@@ -4,3 +4,4 @@ export './temperature_card/temperature_card.dart';
 export './humidity_uv_card/humidity_uv_card.dart';
 export './wind_conditions_card/wind_conditions_card.dart';
 export './astro_data_card/astro_data_card.dart';
+export './aqi_air_pressure_card/aqi_air_pressure_card.dart';
